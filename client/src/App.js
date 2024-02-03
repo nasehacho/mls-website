694da01426ef6a9d9bb84093f2ef70a17feca98d
Shamima Ali <shamimaali@Shamimas-MacBook-Air.local>
2024-02-03 17:14:55 -0330

    all resources page with dummy data

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,13 @@
 import React from 'react'
 import MLSHeader from './components/MLSHeader/MLSHeader';
+import RecResources from './components/RecResources/RecResources';
 
 
 function App() {
   return (
     <>
       <MLSHeader />
+      <RecResources />
     </>
   );
 }
