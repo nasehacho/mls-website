fe55600849cc9e5defe7071f5405b67cc51c2b49
Naseha <nasehacho@gmail.com>
2024-02-03 17:56:50 -0500

    Updates

--- client/src/App.js
+++ client/src/App.js
@@ -2,24 +2,6 @@ import React from 'react'
 import MLSHeader from './components/MLSHeader/MLSHeader';
 import RecResources from './components/RecResources/RecResources';
 
-client
-
-const subjects = [
-  { name: 'Immunohematology', description: 'The study of antigen-antibody reactions associated with blood transfusions', href: '#', icon: ChartPieIcon },
-  { name: 'Immunology', description: 'The study of the immune system', href: '#', icon: CursorArrowRaysIcon },
-  { name: 'Microbiology', description: 'The study of the biology of microscopic organisms', href: '#', icon: FingerPrintIcon },
-  { name: 'Hematology', description: 'The study of blood and blood disorders', href: '#', icon: SquaresPlusIcon },
-  { name: 'Urinalysis', description: 'The study of urine and other bodily fluids', href: '#', icon: ArrowPathIcon },
-  { name: 'Clinical Chemistry', description: 'The study of analysis of chemical substances within the body', href: '#', icon: ArrowPathIcon },
-]
-
-function classNames(...classes) {
-  return classes.filter(Boolean).join(' ')
-}
-
-  export default function App() {
-    const [mobileMenuOpen, setMobileMenuOpen] = useState(false)
-main
 
 function App() {
   return (
---
+++ client/src/components/UploadFile/UploadFile.js
@@ -0,0 +1,33 @@
+js
+import React, { useState } from 'react';
+import axios from 'axios';
+
+const UploadFile = () => {
+   const [selectedFile, setSelectedFile] = useState(null);
+
+   const handleFileUpload = (event) => {
+     setSelectedFile(event.target.files[0]);
+   };
+
+   const handleUpload = () => {
+     const formData = new FormData();
+     formData.append('file', selectedFile);
+     axios.post('/api/upload', formData)
+       .then((response) => {
+         console.log(response.data);
+       })
+       .catch((error) => {
+         console.log(error);
+       });
+   };
+
+   return(
+     <div>
+       <h3>Upload File</h3>
+       <input type="file" onChange={handleFileUpload} />
+       <button onClick={handleUpload}>Upload</button>
+     </div>
+   );
+};
+
+export default UploadFile;
