de18eb338b8aec9878dc25ee2e5ca43acc470500
Shamima Ali <shamimaali@Shamimas-MacBook-Air.local>
2024-02-03 21:15:43 -0330

    added router

--- client/src/index.js
+++ client/src/index.js
@@ -2,12 +2,20 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import UploadFile from './components/UploadFile/UploadFile'
 import reportWebVitals from './reportWebVitals';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<App/>} />
+      <Route path="/home" element={<App/>} />
+      <Route path="/upload" element={<UploadFile/>} />      
+    </Routes>
+    </BrowserRouter>
   </React.StrictMode>
 );
 
